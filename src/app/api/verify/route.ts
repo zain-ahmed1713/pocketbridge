73b5bb106568006e1f47f7f091a8098da737a161
Zain Ahmed <zainahmed1713@gmail.com>
2024-07-22 00:38:38 +0500

    Add verify route to verify user using OTP

---
+++ src/app/api/verify/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server"
+import connect from "@/dbConnect/dbConnect"
+import userModel from "@/models/userModel";
+
+export const POST = async (request: NextRequest) => {
+    await connect();
+    try {
+        const reqBody = await request.json();
+        const {username, verificationCode} = reqBody;
+
+        const user = await userModel.findOne({username});
+        // console.log(new Date(user.verificationTokenExpiry));
+
+        if(!user) {
+            return NextResponse.json({
+                message: "User does not exist"
+            })
+        }
+
+        
+        if(new Date(user.verificationTokenExpiry) > new Date()) {
+            if(user.verificationCode === verificationCode) {
+                await userModel.findByIdAndUpdate(user._id, {isVerified: true});
+                return NextResponse.json({
+                    message: "User verified"
+                })
+            }
+
+            return NextResponse.json({
+                message: "Verification code is wrong"
+            }) 
+        }
+
+        return NextResponse.json({
+            message: "Confirmation time exceeded. Please generate a new code"
+        })
+
+    } catch (error) {
+        return NextResponse.json({
+            message: `Couldn't verify user. Error: ${error}`
+        })
+    }
+}
