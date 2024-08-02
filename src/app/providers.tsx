5069ce5b2ea4345c6d730c9b9a6c2875d02a3d80
Zain Ahmed <zainahmed1713@gmail.com>
2024-08-03 03:10:28 +0500

    Add redux provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { Provider } from "react-redux";
+import store, { persistor } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
+}
